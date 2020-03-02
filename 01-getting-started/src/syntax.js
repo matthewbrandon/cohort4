7aac0ea5fa1c151184cc134cfd401aa970ee3a22
Matthew Brandon Thompson <matthew.brandon.thompson@gmail.com>
2020-03-02 13:20:54 -0700

    Updates for 100C

---
+++ 01-getting-started/src/syntax.js
@@ -0,0 +1,28 @@
+// 100C Syntax Exercise
+// Matthew Brandon Thompson March 2 2020
+
+// define attributes / variables
+// number
+// string
+// boolean
+// array
+// dictionary / objects
+// undefined
+// sample if / else
+// functions
+// parameters
+// returns
+// arrays
+// add to the front
+// add to the end
+// update values
+// loops 
+// for
+// for/in
+// while
+// do while
+// forEach (with array and function)
+// Objects / Dictionaries
+// declare object
+// lookup key to retrieve value
+
