76554b991ebc57a0defc02ed1b4fe1065e20b7df
Matthew Brandon Thompson <matthew.brandon.thompson@gmail.com>
2020-03-02 10:35:43 -0700

    Revisions for 100C

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -14,9 +14,10 @@ const functions = {
 
     subtract: (num1, num2) => {
         return num1 - num2;
-    }
-
+    },
     
 };
 
 export default functions;
+
+
--- 01-getting-started/src/scripts/functions.test.js
+++ 01-getting-started/src/scripts/functions.test.js
@@ -16,3 +16,4 @@ test('Does that add function work?', () => {
     expect(functions.add(1,2)).toBe(3);
     expect(functions.add(101,202)).toBe(303);
 });
+
