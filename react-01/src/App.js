667c8dff00db3f6e532527b228b9e4189fe1d260
Matthew Brandon Thompson <matthew.brandon.thompson@gmail.com>
2020-04-08 14:38:49 -0600

    Working through 140A

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myState: "TBD"
+    };
+    this.counter = 21;
+  }
+  onPushMe = () => {
+    console.log("You Pushed Me");
+    this.counter ++;
+    this.setState({
+      myState: "now:" + this.counter
+    });
+    console.log("Counter is: " + this.counter);
+  }
   render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>I am in control of this application and my name is Matthew</h1>
+        <h1>I am in control of this application and my name is Matthew {this.counter} {this.state.myState}</h1>
+        <button onClick={this.onPushMe}>
+          Push Me
+        </button>
+        <MyComponent
+         
+          />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
